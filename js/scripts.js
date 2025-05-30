7949db709471ddd900706f66b0a93ebea18e5d05
alberurr <82971833+alberurr@users.noreply.github.com>
2025-05-30 12:38:23 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".participante-foto").forEach(function (img) {
         img.addEventListener("click", function () {
             var modalImg = document.getElementById("imagenModalSrc");
-            modalImg.src = this.src; // Asigna la imagen clicada al modal
+            
+            // Verifica que la imagen existe antes de asignarla
+            if (modalImg && this.src) {
+                modalImg.src = this.src;
+                modalImg.alt = this.alt; // También actualiza el texto alternativo
+            } else {
+                console.error("No se pudo actualizar la imagen en el modal.");
+            }
         });
     });
 });
