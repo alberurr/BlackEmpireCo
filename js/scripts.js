0568abc99e7fa599f4902ade644c094e6d520536
alberurr <82971833+alberurr@users.noreply.github.com>
2025-05-31 08:47:32 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,44 +5,155 @@ document.addEventListener("DOMContentLoaded", function () {
             modalImg.src = this.src;
             modalImg.alt = this.alt;
 
-            // Datos de rondas y peso por atleta
+            var nombreAtleta = this.closest("tr").querySelector("td:last-child").textContent.trim();
+            console.log("Nombre del atleta:", nombreAtleta);
+
             var datosAtletas = {
-                "Juan Pérez": [
-                    { Intento: 1, peso:  },
-                    { Intento: 2, peso:  },
-                    { Intento: 3, peso:  }
+                "Frida Lopez": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
                 ],
-                "Alejandro Pérez": [
-                    { Intento: 1, peso: },
-                    { Intento: 2, peso: },
-                    { Intento: 3, peso: }
+                "Grecia Anguiano": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
                 ],
-                "Victor Pérez": [
-                    { Intento: 1, peso: },
-                    { Intento: 2, peso: },
-                    { Intento: 3, peso: }
+                "Sonia Serrano": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Mary Jose Rosales": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Mariana Fragoso": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Brenda Mancilla": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Irene Morales": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "David Arciniega": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Oliver Navarro": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Alejandro Chong": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Juan Loaiza": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Adrian Piña": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Javier Valle": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Luis Muñoz": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
+                ],
+                "Hector Mota": [
+                    { Intento: 1, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 2, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 3, peso: 0, levantamiento: "Snatch" },
+                    { Intento: 1, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 2, peso: 0, levantamiento: "Clean & Jerk" },
+                    { Intento: 3, peso: 0, levantamiento: "Clean & Jerk" }
                 ]
             };
 
-            // Obtener el nombre del atleta desde la fila de la tabla
-            var nombreAtleta = this.closest("tr").querySelector("td:last-child").textContent.trim();
+            var datos = datosAtletas[nombreAtleta] || [];
 
-            // Limpiar contenido previo de la tabla
+            // Encontrar los pesos máximos de Snatch y Clean & Jerk
+            var maxSnatch = Math.max(...datos.filter(fila => fila.levantamiento === "Snatch").map(fila => fila.peso));
+            var maxCleanJerk = Math.max(...datos.filter(fila => fila.levantamiento === "Clean & Jerk").map(fila => fila.peso));
+
             var tablaBody = document.getElementById("tablaModalBody");
             tablaBody.innerHTML = "";
 
-            // Obtener los datos correspondientes al atleta
-            var datos = datosAtletas[nombreAtleta] || [];
-
-            // Llenar la tabla con los datos
             datos.forEach(function (fila) {
                 var row = document.createElement("tr");
-                row.innerHTML = `<td>${fila.ronda}</td><td>${fila.peso}</td>`;
+
+                // Aplicar clase de resaltado si el peso es el mayor dentro de su levantamiento
+                var claseResaltado = fila.peso === maxSnatch || fila.peso === maxCleanJerk ? "peso-maximo" : "";
+
+                row.innerHTML = `<td>${fila.Intento}</td><td class="${claseResaltado}">${fila.peso} kg</td><td>${fila.levantamiento}</td>`;
                 tablaBody.appendChild(row);
             });
         });
     });
 });
+
+
 // Cerrar Modal
 function cerrarModal() {
     var modal = bootstrap.Modal.getInstance(document.getElementById("imagenModal"));
